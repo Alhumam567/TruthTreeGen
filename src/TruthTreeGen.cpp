6263391f37d7f235848945b8d33b81aa3f00e23a
Alhumam567 <alhumam567@gmail.com>
2021-04-29 22:08:18 -0400

    Working on TxtCtrl event handling

--- src/TruthTreeGen.cpp
+++ src/TruthTreeGen.cpp
@@ -22,38 +22,61 @@ std::string specialChars[9]{
     "\u2194"
 };
 
+// void handleTxtCtrlFocus(wxFocusEvent &fe) {
+//     std::cout << fe.GetEventObject();
+// }
+
 // A sample app that adds the scrolled pane to a frame to make this code runnable
 class TruthTreeApp: public wxApp
 {
-    wxFrame *frame;
 public:
     bool OnInit();
 };
 
-IMPLEMENT_APP(TruthTreeApp);
 
-bool TruthTreeApp::OnInit()
+class TruthTreeFrame: public wxFrame 
+{
+public:
+    TruthTreeFrame();
+};
+
+class SymbolButton: public wxButton 
+{
+    wxButton *btn;
+public:
+    SymbolButton(wxFrame *frame, wxString specialChar, wxTextCtrl *txt);
+};
+
+
+
+SymbolButton::SymbolButton(wxFrame *frame, wxString specialChar, wxTextCtrl *txt) : 
+    wxButton(frame, wxID_ANY, specialChar, wxDefaultPosition, wxSize(20,20))
+{
+    std::cout << "Char: " << specialChar << " initialized\n";
+}
+
+TruthTreeFrame::TruthTreeFrame() : 
+    wxFrame((wxFrame *)NULL, -1,  wxT("Truth Tree Generator"), wxPoint(100,50), wxSize(650,650))
 {
     wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
-    frame = new wxFrame((wxFrame *)NULL, -1,  wxT("Scrolling Widgets"), wxPoint(50,50), wxSize(650,650));
-    
+
     wxGridBagSizer* inputSizer = new wxGridBagSizer(2,2);
-    wxTextCtrl* argTextbox = new wxTextCtrl(frame, -1, "default arguments", wxDefaultPosition, wxSize(350,20)); 
-    wxStaticText* argText = new wxStaticText(frame, -1, "Arguments: ");
-    wxTextCtrl* concTextbox = new wxTextCtrl(frame, -1, "default conclusion", wxDefaultPosition, wxSize(200,20)); 
-    wxStaticText* concText = new wxStaticText(frame, -1, "Conclusion: ");
+    wxTextCtrl* argTextbox = new wxTextCtrl(this, -1, "default arguments", wxDefaultPosition, wxSize(350,20)); 
+    wxStaticText* argText = new wxStaticText(this, -1, "Arguments: ");
+    wxTextCtrl* concTextbox = new wxTextCtrl(this, -1, "default conclusion", wxDefaultPosition, wxSize(200,20)); 
+    wxStaticText* concText = new wxStaticText(this, -1, "Conclusion: ");
     inputSizer->Add(argText, wxGBPosition(0, 0));
     inputSizer->Add(argTextbox, wxGBPosition(0, 1));
     inputSizer->Add(concText, wxGBPosition(1, 0));
     inputSizer->Add(concTextbox, wxGBPosition(1, 1));
     
     wxBoxSizer* btnSizer = new wxBoxSizer(wxHORIZONTAL);
-    wxButton* charBtns[9];
+    SymbolButton* charBtns[9];
     
     int i = 0;
     for (auto &btn : charBtns) {
         wxString mystring(specialChars[i++].c_str(), wxConvUTF8);
-        btn = new wxButton(frame, wxID_ANY, mystring, wxDefaultPosition, wxSize(20,20));
+        btn = new SymbolButton(this, mystring, concTextbox);
     }
     for (int i = 0; i<9; i++) 
         btnSizer->Add(charBtns[i], 0, wxALL, 2);
@@ -61,10 +84,18 @@ bool TruthTreeApp::OnInit()
     mainSizer->Add(btnSizer, 0, wxEXPAND);
     mainSizer->Add(inputSizer, 0, wxEXPAND);
 
-    ScrolledWidgetsPane* my_image = new ScrolledWidgetsPane(frame, wxID_ANY);
+    ScrolledWidgetsPane* my_image = new ScrolledWidgetsPane(this, wxID_ANY);
     mainSizer->Add(my_image, 1, wxEXPAND);
     
-    frame->SetSizer(mainSizer);
-    frame->Show();
+    this->SetSizer(mainSizer);
+    this->Show();
+}
+
+IMPLEMENT_APP(TruthTreeApp);
+
+bool TruthTreeApp::OnInit()
+{
+    TruthTreeFrame *frame = new TruthTreeFrame();
+    frame->Show(true); 
     return true;
 } 
