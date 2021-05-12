3d8422aef07adab8d4714cb3fcf86e18d1405b5e
Alhumam567 <alhumam567@gmail.com>
2021-05-12 03:45:24 -0400

    Model constructor

--- src/TruthTreeModel.cpp
+++ src/TruthTreeModel.cpp
@@ -23,15 +23,19 @@ int update(std::string statement, std::vector<std::string> newStatements) {
 TruthTreeModel::TruthTreeModel(std::vector<std::string> arguments, std::string conclusion) : 
     complete {false}
 {
+    this->arguments = arguments;
     if (!DecompositionUtil::isLiteral(conclusion)) {
-        conclusion = "\uFFE2(" + conclusion + ")";
+        this->conclusion = "\uFFE2(" + conclusion + ")";
     } else {
-        conclusion = "\uFFE2" + conclusion ;
+        this->conclusion = "\uFFE2" + conclusion ;
     }
+
+    arguments.push_back(conclusion);
+    branches = {new TruthTreeBranch(arguments)}; 
 }
 
 TruthTreeModel::~TruthTreeModel() {
-    
+    // delete all truth tree branches
 }
 
 int TruthTreeModel::generateTree() {
@@ -42,4 +46,6 @@ void TruthTreeModel::applyDecompositionRule(TruthTreeBranch branch, std::string 
     std::vector<std::string> *decomposition = new std::vector<std::string>{};
 
     bool split = DecompositionUtil::decompose(statement, decomposition);
+
+    branch.update(statement, *decomposition);
 }
